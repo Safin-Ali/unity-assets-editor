778664f39080d43b8ba5bbc234a082b4c9088d23
Safin-Ali <safin.ali.7205@gmail.com>
2024-10-10 19:16:45 +0600

    refactor: :art: app entry point refectored

--- src/app.js
+++ src/app.js
@@ -1,6 +1,3 @@
-import FileHandler from "./classes/FileHandler.js";
-import HexHandler from "./classes/HexHandler.js";
 import { CLIHandler } from "./classes/CLIHandler.js";
-import { asciiToHexBytes, pathGen } from "./utils/common-utils.js";
 
 new CLIHandler();
