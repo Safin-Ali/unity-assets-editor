f40c1e717483369f32ea4f8c724210524e9fd097
Safin-Ali <safin.ali.7205@gmail.com>
2024-10-10 15:45:48 +0600

    fix: :bug: FileHandler Class destPath issue fixed

--- src/classes/FileHandler.js
+++ src/classes/FileHandler.js
@@ -1,46 +1,55 @@
-import { readFileSync, writeFileSync } from 'node:fs';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'node:fs';
+import { join, parse } from 'node:path';
 
 /**
  * Handles file operations, including reading from and writing to files.
  */
 export default class FileHandler {
-    /**
-     * Creates an instance of FileHandler.
-     * 
-     * @param {Object} options - Configuration options for the FileHandler.
-     * @param {string} options.inputPath - Path to the input file to read.
-     * @param {string} [options.outPath=''] - Path to the output file to write. Defaults to an empty string.
-     * @throws {Error} Throws an error if the input file cannot be read.
-     */
-    constructor({ inputPath, outPath = '' }) {
-        if (typeof inputPath !== 'string') {
-            throw new TypeError('The inputPath must be a string.');
-        }
+	/**
+	 * Creates an instance of FileHandler.
+	 * 
+	 * @param {Object} options - Configuration options for the FileHandler.
+	 * @param {string} options.inputPath - Path to the input file to read.
+	 * @param {string} [options.outPath=''] - Path to the output file to write. Defaults to an empty string.
+	 * @throws {Error} Throws an error if the input file cannot be read.
+	 */
 
-        this.destPath = outPath;
+	#destPath = null;
+	constructor({ inputPath, outPath, fileName }) {
+		if (typeof inputPath !== 'string') {
+			throw new TypeError('The inputPath must be a string.');
+		}
 
-        try {
-            const fileContent = readFileSync(inputPath, 'hex');
-            this.buffer = (fileContent.toUpperCase().match(/.{1,2}/g) || []);
-        } catch (error) {
-            throw new Error(`Failed to read file at ${inputPath}: ${error.message}`);
-        }
-    }
+		this.#destPath = outPath;
 
-    /**
-     * Writes the buffer to the specified output file in hexadecimal format.
-     * @throws {Error} Throws an error if the file cannot be written.
-     */
-    writeBuffer() {
-        if (typeof this.destPath !== 'string') {
-            throw new TypeError('The destPath must be a string.');
-        }
+		try {
+			const fileContent = readFileSync(inputPath, 'hex');
+			this.buffer = (fileContent.toUpperCase().match(/.{1,2}/g) || []);
+		} catch (error) {
+			throw new Error(`Failed to read file at ${inputPath}: ${error.message}`);
+		}
+	}
 
-        try {
-            const hexString = this.buffer.join('').toLowerCase();
-            writeFileSync(this.destPath, hexString, 'hex');
-        } catch (error) {
-            throw new Error(`Failed to write file at ${this.destPath}: ${error.message}`);
-        }
-    }
+	/**
+	 * Writes the buffer to the specified output file in hexadecimal format.
+	 * @throws {Error} Throws an error if the file cannot be written.
+	 */
+	writeBuffer() {
+		const { dir, name } = parse(this.#destPath);
+		if (typeof this.#destPath !== 'string') {
+			throw new TypeError('The destPath must be a string.');
+		}
+
+		try {
+			const hexString = this.buffer.join('').toLowerCase();
+
+			if (!existsSync(this.#destPath)) {
+				mkdirSync(dir, { recursive: true });
+			}
+			
+			writeFileSync(join(dir, name), hexString, 'hex');
+		} catch (error) {
+			throw new Error(`Failed to write file at ${this.#destPath}: ${error.message}`);
+		}
+	}
 }
