401befa9c532ab69e2451f26b6215abee2bbb46d
Safin-Ali <safin.ali.7205@gmail.com>
2024-10-14 20:43:54 +0600

    style: :lipstick: a new package deno_figlet used for ascii draw in cli
    entire application little patched with ascii draw

--- src/app.js
+++ src/app.js
@@ -1,8 +1,16 @@
+import { brightBlue, brightRed } from "https://deno.land/std@0.221.0/fmt/colors.ts";
 import { CLIHandler } from "./classes/CLIHandler.js";
-import { appEmitter } from "./event/app-event.js";
+import { appEmitter, restartApp } from "./event/app-event.js";
+import { textCB } from "https://deno.land/x/deno_figlet@1.0.0/mod.ts";
+import "https://deno.land/x/deno_figlet@1.0.0/dist/fonts/_doom.ts";
+import { brightGreen } from "https://deno.land/std@0.170.0/fmt/colors.ts";
 
 appEmitter.on("restart", () => {
-  new CLIHandler();
+	console.log(`${brightGreen("Created by")} ${brightRed("DevSA")}`)
+  textCB("UABE BUSSID", "doom", null, (myAwesomeFiglet) => {
+    console.log(brightBlue(myAwesomeFiglet));
+    new CLIHandler();
+  });
 });
 
-new CLIHandler();
+restartApp();
